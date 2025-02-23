4c821f7ad5e2a190e535abc1e67cbabfa0a1e4d2
Maria Salazar <mariasalazarsdl@gmail.com>
2025-02-23 23:57:49 +0000

    Actualizada la clase "Invader"
    He actualizado la clase "Invader" añadiendo su código lógico de movimientos e interacciones.

--- Código/SpaceInvadersJava/src/Invader.java
+++ Código/SpaceInvadersJava/src/Invader.java
@@ -1,2 +1,39 @@
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Rectangle;
+
 public class Invader {
+    private int x, y;
+    private int ancho = 30, alto = 20;
+    private int velocidad = 2;
+    private boolean moviendoDerecha = true;
+
+    public Invader(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Movimiento horizontal
+    public void actualizar() {
+        if(moviendoDerecha) {
+            x += velocidad;
+        } else {
+            x -= velocidad;
+        }
+    }
+
+    // Baja un poco al cambiar de dirección
+    public void cambiarDireccion() {
+        moviendoDerecha = !moviendoDerecha;
+        y += alto;
+    }
+
+    public void dibujar(Graphics g) {
+        g.setColor(Color.RED);
+        g.fillRect(x, y, ancho, alto);
+    }
+
+    public Rectangle obtenerRectangulo() {
+        return new Rectangle(x, y, ancho, alto);
+    }
 }

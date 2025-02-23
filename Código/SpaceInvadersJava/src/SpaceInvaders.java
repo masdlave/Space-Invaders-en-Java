cf4a706096a68b927f459045425c4ca6ac7c7616
Maria Salazar <mariasalazarsdl@gmail.com>
2025-02-23 23:24:51 +0000

    Actualizada la clase principal
    He actualizado la clase principal "SpaceInvaders" con las declaraciones básicas iniciales (pertenecientes al "lesson01")

--- Código/SpaceInvadersJava/src/SpaceInvaders.java
+++ Código/SpaceInvadersJava/src/SpaceInvaders.java
@@ -1,2 +1,18 @@
+// La clase principal es esta (SpaceInvaders), no Main
+import javax.swing.JFrame;
+
 public class SpaceInvaders {
+    public static void main(String[] args) {
+        JFrame ventana = new JFrame("Space Invaders");
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setResizable(false);
+
+        GamePanel panel = new GamePanel();
+        ventana.add(panel);
+        ventana.pack();
+        ventana.setLocationRelativeTo(null);
+        ventana.setVisible(true);
+
+        panel.iniciarJuego();
+    }
 }

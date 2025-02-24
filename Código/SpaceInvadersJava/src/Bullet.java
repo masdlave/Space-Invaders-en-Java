451872c39b4422d1d15ac4aa245b27c047ef9976
Maria Salazar <mariasalazarsdl@gmail.com>
2025-02-24 00:06:14 +0000

    Actualizada la clase "Bullet"
    He establecido el código correspondiente para que el "disparo2 sea funcional (Para hacerlo funcionar, debemos implementar esta clase en el "GamePanel") - Lesson 05

--- Código/SpaceInvadersJava/src/Bullet.java
+++ Código/SpaceInvadersJava/src/Bullet.java
@@ -1,2 +1,31 @@
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Rectangle;
+
 public class Bullet {
+    private int x, y;
+    private int ancho = 4, alto = 10;
+    private int velocidad = 7;
+
+    public Bullet(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void actualizar() {
+        y -= velocidad;
+    }
+
+    public void dibujar(Graphics g) {
+        g.setColor(Color.YELLOW);
+        g.fillRect(x, y, ancho, alto);
+    }
+
+    public Rectangle obtenerRectangulo() {
+        return new Rectangle(x, y, ancho, alto);
+    }
+
+    public int getY() {
+        return y;
+    }
 }
